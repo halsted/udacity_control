f1827b0eff2e184ac30102ac882a7969ece2af35
halsted <lehalsted@ucdavis.edu>
2022-10-20 11:29:49 -0700

    Add files via upload

---
+++ pid_controller.cpp
@@ -0,0 +1,61 @@
+/**********************************************
+ * Self-Driving Car Nano-degree - Udacity
+ *  Created on: December 11, 2020
+ *      Author: Mathilde Badoual
+ **********************************************/
+
+#include "pid_controller.h"
+#include <vector>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, double output_lim_mini) {
+   /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   **/
+  tau_p = Kpi;
+  tau_i = Kii;
+  tau_d = Kdi;
+  output_lim_max = output_lim_maxi;
+  output_lim_min = output_lim_mini;
+  prev_cte=0;  
+  int_cte=0;
+}
+
+
+void PID::UpdateError(double cte) {
+   /**
+   * TODO: Update PID errors based on cte.
+   **/
+  cte = cte;
+  diff_cte = (cte - prev_cte) / dt;
+  int_cte += cte;
+  prev_cte = cte;  
+}
+
+double PID::TotalError() {
+   /**
+   * TODO: Calculate and return the total error
+    * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
+   */
+    double control = tau_p*cte + tau_i*int_cte + tau_d*diff_cte;
+    if (control < output_lim_min) {
+      control = output_lim_min; 
+    } else if (control > output_lim_max) {
+      control = output_lim_max;
+    }
+    return control;
+}
+
+double PID::UpdateDeltaTime(double new_delta_time) {
+   /**
+   * TODO: Update the delta time with new value
+   */
+   dt = new_delta_time;
+}
